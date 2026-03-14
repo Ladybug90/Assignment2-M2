113a13d3d69083e9dcab43fef6b34f2b79dd2e2d
Sannteen Budda <sannteenbudda@gmail.com>
2026-03-14 15:12:13 -0500

    js added

---
+++ Question2/script.js
@@ -0,0 +1,30 @@
+const paypal = document.getElementById("paypal");
+const cardFields = [
+  document.getElementById("cardNumber"),
+  document.getElementById("expiry"),
+  document.getElementById("cvv")
+];
+
+document.querySelectorAll("input[name='payment']").forEach(radio => {
+  radio.addEventListener("change", function () {
+
+    if (paypal.checked) {
+      cardFields.forEach(field => field.disabled = true);
+    } else {
+      cardFields.forEach(field => field.disabled = false);
+    }
+
+  });
+});
+
+// Shipping address toggle
+const sameBillingCheckbox = document.getElementById("sameBilling");
+const shippingFields = document.getElementById("shippingFields");
+
+sameBillingCheckbox.addEventListener("change", function () {
+    if (sameBillingCheckbox.checked) {
+        shippingFields.style.display = "none";
+    } else {
+        shippingFields.style.display = "block";
+    } 
+});
